f9b5e60d33bbe6113b0c7794be7aaafee8f08b79
Ziroux <ziroux@ziroux.net>
2025-01-30 10:21:24 +0100

    todo

--- contributing.md
+++ contributing.md
@@ -5,8 +5,9 @@ Testing Locally:
 ```shell
 asdf plugin test <plugin-name> <plugin-url> [--asdf-tool-version <version>] [--asdf-plugin-gitref <git-ref>] [test-command*]
 
-# TODO: adapt this
 asdf plugin test tfsort https://github.com/ziroux/asdf-tfsort.git "tfsort --help"
+# or
+asdf plugin test asdf-tfsort https://github.com/ziroux/asdf-tfsort --asdf-plugin-gitref my_branch "tfsort --help"
 ```
 
 Tests are automatically run in GitHub Actions on push and PR.
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for tfsort.
 GH_REPO="https://github.com/AlexNabokikh/tfsort"
 TOOL_NAME="tfsort"
 TOOL_TEST="tfsort --help"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if tfsort has other means of determining installable versions.
 	list_github_tags
 }
 
